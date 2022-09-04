59dd824dc8ecc2a4d823fd65949120ae2268739e
Diana/Dinorekkos <diana_ramos_g@hotmail.com>
2022-09-03 22:45:54 -0500

    fix viewport x2

--- HyperCats_DianaRG/Assets/PunkyCats/Code/UI/ClosetController.cs
+++ HyperCats_DianaRG/Assets/PunkyCats/Code/UI/ClosetController.cs
@@ -6,20 +6,25 @@ using UnityEngine;
 
 public class ClosetController : MonoBehaviour
 {
-    [SerializeField] private List<AssetViwer> assetsViwers;
+    [SerializeField] private AssetViwer itemPrefab;
+    [SerializeField] private RectTransform contentPanel;
+    
+    [SerializeField] private List<AssetViwer> itemsInv;
+
+    [SerializeField] private CurrencyType filterAssets;
     void Start()
     {
-        SaveManager.Instance.OnUnlockAsset += CreateItemsInventory;
+        // SaveManager.Instance.OnUnlockAsset += InitializeInventoryUI;
     }
 
     private void OnEnable()
     { 
-        SaveManager.Instance.OnUnlockAsset += CreateItemsInventory;
+        // SaveManager.Instance.OnUnlockAsset += InitializeInventoryUI;
     }
 
     private void OnDisable()
     {
-        SaveManager.Instance.OnUnlockAsset -= CreateItemsInventory;
+        // SaveManager.Instance.OnUnlockAsset -= InitializeInventoryUI;
 
     }
 
@@ -29,21 +34,23 @@ public class ClosetController : MonoBehaviour
         
     }
 
-    public void CreateItemsInventory()
+    public void InitializeInventoryUI()
     {
         List<Asset_SO> unlockdeAssets = SaveManager.Instance.UnlockedAssets;
+     
         for (int i = 0; i < unlockdeAssets.Count; i++)
         {
-            
+            AssetViwer assetViwer = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity);
+            assetViwer.transform.SetParent(contentPanel);
+            itemsInv.Add(assetViwer);
+
+            Asset_SO assetSo = unlockdeAssets[i]; 
+            Debug.Log(assetSo.ID); 
+            assetViwer.myAsset_Data = assetSo;
+            assetViwer.CatUI.SetAssetViwer(assetSo.AssetType, assetSo.Sprite);
         }
-        foreach (Asset_SO assetData in unlockdeAssets)
-        {
-            
-        }
-        foreach (AssetViwer viewer in assetsViwers)
-        {
-            viewer.CatUI.ResetAssets();
-        }
+        
+        
     }
     
 }
